dbb6a447f1c4935d960bc6b8fa605e0258645a9a
cawley <47256354+cawley@users.noreply.github.com>
2023-07-18 15:23:51 -0400

    .

--- curves/demo.cpp
+++ curves/demo.cpp
@@ -1,5 +1,46 @@
-#include <iostream> 
+#include <fstream>
+#include <sstream>
+#include <queue>
+#include <vector>
 
-int main(){
-    cout << "test\n";
+using namespace std;
+
+// Function to split a string by a delimiter
+vector<string> split(const string &s, char delimiter) {
+    vector<string> tokens;
+    string token;
+    istringstream tokenStream(s);
+    while (getline(tokenStream, token, delimiter)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+int main() {
+    ifstream file("data.csv");
+    vector<queue<string>> columns;
+    string line;
+
+    // Read the first line to determine the number of columns
+    if (getline(file, line)) {
+        vector<string> firstRow = split(line, ',');
+        columns.resize(firstRow.size());
+
+        for (size_t i = 0; i < firstRow.size(); ++i) {
+            columns[i].push(firstRow[i]);
+        }
+    }
+
+    // Read the rest of the lines
+    while (getline(file, line)) {
+        vector<string> row = split(line, ',');
+
+        for (size_t i = 0; i < row.size(); ++i) {
+            columns[i].push(row[i]);
+        }
+    }
+
+    file.close();
+    
+    return 0;
 }
