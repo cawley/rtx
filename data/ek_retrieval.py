96c50b47e41e69371fdfd74050d8608848bc840c
cawley <47256354+cawley@users.noreply.github.com>
2023-07-19 17:29:36 -0400

    ek_updates

--- data/ek_retrieval.py
+++ data/ek_retrieval.py
@@ -1,17 +1,17 @@
 import eikon as ek
 import pandas as pd
 
-# Set your Eikon API key
-ek.set_app_key('your_app_key')
+# access with API key
+ek.set_app_key("your_app_key")
 
-# Define the RIC for 10-year US Treasury bonds
-ric = 'US10YT=RR'
+# ric for 10yr bonds
+ric = "US10YT=RR"
 
 # define field (this example has close price)
-fields = ['CLOSE']
+fields = ["CLOSE"]
 
 # fetch historical data
 df, err = ek.get_data(ric, fields)
 
 # port 2 csv
-df.to_csv('10_year_treasury_prices.csv')
+df.to_csv("10_year_treasury_prices.csv")
