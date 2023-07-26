736e40ede30c5f99f197d34c0f80ba58e545cbcd
cawley <47256354+cawley@users.noreply.github.com>
2023-07-26 15:40:19 -0400

    eventus_entry_retrieval

--- data/eventus_entry_retrieval.py
+++ data/eventus_entry_retrieval.py
@@ -6,14 +6,20 @@ df = pd.read_csv("eventus_table_match.csv")
 # ask the user for the filter column name
 filter_col_name = input("Enter the filter column name: ")
 
+# ask the user for the entry
+entry = input("Enter the entry in the filter column: ")
+
 # ask the user for the matching column name
 match_col_name = input("Enter the matching column name: ")
 
-# get a list of the unique values in the filter column
-filter_col_values = df[filter_col_name].unique()
+# get all the rows where the filter column equals the entry
+df_a = df[df[filter_col_name] == entry]
+
+# get a list of the values in the matching column for these rows
+match_col_values = df_a[match_col_name].tolist()
 
 # get all the rows where the matching column values are in the list
-df_b = df[df[match_col_name].isin(filter_col_values)]
+df_b = df[df[match_col_name].isin(match_col_values)]
 
 # save the result to a new csv file
 df_b.to_csv(
